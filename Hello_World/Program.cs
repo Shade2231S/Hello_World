d65200357ce4dff441da80bd3855945f0c45c276
Shade2231S <matthewmcculloch149@gmail.com>
2026-09-09 14:52:25 -0300

    Initial commit

---
+++ Hello_World/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    internal class Program
+    {
+        static void Main2()
+        {
+            Console.WriteLine("Shade!");
+        }
+        //static void Main(string[] args)
+        static void Main()
+        {
+            Console.WriteLine("Hello World :3");
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to leave program");
+        }
+    }
+}
